6973a095e988fa5fa058acf227cbf10bf208f625
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-03-20 09:50:24 +0100

    linux.tcl: add support for mesa_gpu-cpu
    When <gpu> node is present in runtime, add fs_rom + vfs to route
    "mesa_gpu_drv.lib.so" to "mesa_gpu-softpipe.lib.so" for software
    rendering.

--- share/goa/lib/run/linux.tcl
+++ share/goa/lib/run/linux.tcl
@@ -265,6 +265,16 @@ proc generate_runtime_config { } {
 		                     "</service>"
 	}
 
+	set mesa_route ""
+	catch {
+		set gpu_node [query_node /runtime/requires/gpu $runtime_file]
+
+		append mesa_route "\n\t\t\t\t\t" \
+			"<service name=\"ROM\" label=\"mesa_gpu_drv.lib.so\"> " \
+			"<parent label=\"mesa_gpu-softpipe.lib.so\"/> " \
+			"</service>"
+	}
+
 	install_config {
 		<config>
 			<parent-provides>
@@ -299,7 +309,8 @@ proc generate_runtime_config { } {
 				<provides>} $uplink_provides {</provides>
 				<route>} $config_route $gui_route \
 				         $capture_route $event_route \
-				         $nic_route $fs_routes $rtc_route {
+				         $nic_route $fs_routes $rtc_route \
+				         $mesa_route {
 					<service name="ROM">   <parent/> </service>
 					<service name="PD">    <parent/> </service>
 					<service name="RM">    <parent/> </service>
@@ -354,6 +365,11 @@ proc generate_runtime_config { } {
 
 		lappend runtime_archives "nfeske/src/linux_rtc_drv"
 	}
+
+	if {$mesa_route != ""} {
+		lappend rom_modules mesa_gpu-softpipe.lib.so
+	}
+
 
 	lappend runtime_archives "nfeske/src/init"
 	lappend runtime_archives "nfeske/src/base-linux"
